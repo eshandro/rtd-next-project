86c63b6f73edd77bcc64875a6eb692c092492b75
eshandro <erik843@gmail.com>
2017-11-05 20:28:29 -0700

    Parse html for last modified date

--- src/scripts/checkLastModified.js
+++ src/scripts/checkLastModified.js
@@ -18,17 +18,38 @@ function fetchHTML(url) {
 			return res.text();
 		})
 		.then((body) => {
+			let 	test = /\d\d\-\w{3}\-\d{4}\s*\d{2}\:\d{2}/,	
+					temp = body,
+					start = temp.indexOf('google_transit.zip'),
+					end = temp.indexOf('</tr', start),
+					lastModified = "";
+			temp = temp.substring(start,end);
+			lastModified = temp.match(test);
+			console.log("lastModified ",lastModified);
 			return body;
 		})
 		.catch((err) => {
+			console.log("fetchHTML err", err);
 			return err;
 		});
 }
 
+function getLastModifiedFromHTML(html) {
+
+}
+
+function getLastModified() {
+
+}
+
+function updateLastModified() {
+
+}
+
 function checkLastModified(mainFeedUrl) {
 	let body;
-	let data = fetchHTML(mainFeedUrl);
-	data.then((res) => {console.log("res ",res);});
+	fetchHTML(mainFeedUrl);
+	// .then((res) => {console.log("res ",res);});
 			// .then((res) => {
 		// 	console.log("res in checkLastModified ",res);
 		// })
