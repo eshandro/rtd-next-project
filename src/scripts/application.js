cfde81640cf5d0dea38ee28d5e3d3434b9ec9779
eshandro <erik843@gmail.com>
2017-12-22 12:25:55 -0700

    Update package.json and add updateStaticFeed func
    Remove unused packages in package.json
    Add new funciton to application.js - updateStaticFeed and call func

--- src/scripts/application.js
+++ src/scripts/application.js
@@ -8,42 +8,45 @@ const	mainFeedUrl = "http://www.rtd-denver.com/GoogleFeeder/",
 		downloadFolder = __dirname + "/../temp-feed/",
 		extractedFolder = __dirname + "/../feed/";
 
-checkFeed(mainFeedUrl)
-	.then((checkFeedData) => {
-		console.log('data in checkFeed call:', checkFeedData);
-		if (!checkFeedData.needUpdate) {
-			console.log('no update needed on checkFeed: ', checkFeedData.data);
-			return ({downloadFeedSuccess: false, data:checkFeedData.data});
-		} 
-		console.log("checkFeed a success, calling downloadFeed");
-		return downloadFeed(feedUrl, downloadFolder);
+function updateStaticFeed (staticFeedUrl) {
+	checkFeed(staticFeedUrl)
+		.then((checkFeedData) => {
+			console.log('data in checkFeed call:', checkFeedData);
+			if (!checkFeedData.needUpdate) {
+				console.log('no update needed on checkFeed: ', checkFeedData.data);
+				return ({downloadFeedSuccess: false, data:checkFeedData.data});
+			} 
+			console.log("checkFeed a success, calling downloadFeed");
+			return downloadFeed(feedUrl, downloadFolder);
 
-	})
-	.then((downloadData) => {
-		if(!downloadData.downloadFeedSuccess) {
-			console.log("Feed not downloaded: ", downloadData.data);
-			return ({unzipSuccess:false, data:downloadData.data});
-		}
-		console.log("downloadFeed a success, calling unzipFiles");
-		return unzipFiles(downloadData.data, extractedFolder);
-	})
-	.then((unzipData) => {
-		if (!unzipData.unzipSuccess) {
-			console.log('unzipFiles err: ', unzipData.data);
-			return ({updateFeedSuccess: false, data:unzipData.data});
-		} else {
-			console.log("unzipFiles a success, calling updateFeed");
-			return updateFeed(unzipData.data);
-		}
-	})
-	.then((updateFeedData) => {
-		if(!updateFeedData.updateFeedSuccess) {
-			console.log("updateFeed err ",updateFeedData.data);
-			return;
-		}
-		console.log("updateFeed a success, static feed done being updated");
-	})
-	.catch((err) => {
-		console.log("error in checkFeed: ", err);
-	});
+		})
+		.then((downloadData) => {
+			if(!downloadData.downloadFeedSuccess) {
+				console.log("Feed not downloaded: ", downloadData.data);
+				return ({unzipSuccess:false, data:downloadData.data});
+			}
+			console.log("downloadFeed a success, calling unzipFiles");
+			return unzipFiles(downloadData.data, extractedFolder);
+		})
+		.then((unzipData) => {
+			if (!unzipData.unzipSuccess) {
+				console.log('unzipFiles err: ', unzipData.data);
+				return ({updateFeedSuccess: false, data:unzipData.data});
+			} else {
+				console.log("unzipFiles a success, calling updateFeed");
+				return updateFeed(unzipData.data);
+			}
+		})
+		.then((updateFeedData) => {
+			if(!updateFeedData.updateFeedSuccess) {
+				console.log("updateFeed err ",updateFeedData.data);
+				return;
+			}
+			console.log("updateFeed a success, static feed done being updated");
+		})
+		.catch((err) => {
+			console.log("error in checkFeed: ", err);
+		});
+}
+updateStaticFeed(mainFeedUrl);
 
