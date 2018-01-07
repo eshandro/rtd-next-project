8f1622a5d95833b4436591afc801ad8724f2f5f2
eshandro <erik843@gmail.com>
2018-01-06 19:27:58 -0700

    Start parseTxtToJS to prep for writing to db instead of file system

--- src/scripts/parseTxtFileToJS.js
+++ src/scripts/parseTxtFileToJS.js
@@ -1,40 +1,110 @@
 const fs = require('fs'),
-		readFile = require('fs-readfile-promise');
+		readline = require('readline');
+
 
 // May want to refactor to async stream: https://coderwall.com/p/ohjerg/read-large-text-files-in-nodejs
-function parseTxtFileToJS (path) {
-	let 	lines,
-			dataName = path.substring(path.lastIndexOf('/')+1);
-	dataName = dataName.substring(0,dataName.lastIndexOf('.'));
-	return readFile(path, 'utf8')
-		.then((file) => {
-			let keys;
-			lines = file.toString().split('\r\n');
 
-			if (lines && lines.length > 0) {
-				// first line shoule be the keys
-				keys = convertLineToArray(lines[0]);
-				for (let i = 1; i < lines.length; i++) { 
-					if (lines[i].length === 0) {
-						lines.splice(i,1);
-					} else {
-						lines[i] = convertLineToArray(lines[i]);						
+function parseTxtFileToJS (path) {
+	let 	keys,
+			counter = 0,
+			tempArr = [],
+			dataName = path.substring(path.lastIndexOf('/')+1);
+	
+	// jsonPath = path.substring(0,path.lastIndexOf('/')+1) + "json/";
+	dataName = dataName.substring(0,dataName.lastIndexOf('.'));
+
+	let	instream = fs.createReadStream(path),
+			// file = fs.createWriteStream(jsonPath+dataName+".json"),
+			rl = readline.createInterface(instream);
+
+	return new Promise((resolve,reject) => {
+		const errorHandler = (error) => {
+			console.log("errorHandler in parseTxtFileToJson rl promise");
+			let errMsg = `Unable to read file ${path}:  ${error}`;
+			reject({parseTxtFileSuccess: false, data: errMsg})
+		};
+		rl
+			.on('line', (line) => {
+				if(counter === 0) {
+					keys = convertLineToArray(line);		
+					console.log("keys", keys);
+				} else {
+					let currLine = convertLineToArray(line);
+					if (currLine && currLine.length > 0) {
+						// if (keys.length === currLine.length) {
+						// 	if(counter > 1) {
+						// 		currJson = ",\n";
+						// 	} else {
+						// 		currJson = "\n";
+						// 	}
+						// 	let len = keys.length,
+						// 		 i = 0;
+						// 	for (; i < len; i++){
+						// 		if (i === 0) {
+						// 			currJson = `${currJson}\t{`;
+						// 		}
+						// 		currJson = `${currJson}"${keys[i]}":"${currLine[i]}"`
+						// 		if (i !== len-1) {
+						// 			currJson = currJson + ",";
+						// 		} else if (i === len-1) {
+						// 			currJson = currJson + "}"
+						// 		}
+						// 	}
+						// }
+						tempArr.push(convertLineToObj(currLine,keys));
 					}
 				}
-			}
-			let json = convertLinesToJS(lines,keys,dataName);
-			return ({parseTxtFileSuccess: true, data: json});
-		})
-		.catch((err) => {
-			console.log(err);
-			let errMsg = `Error in parseTxtFileToJson readFile: err.code=${err.code} on file ${err.path}.`;
-			console.log("errMsg ",errMsg);
-			return ({parseTxtFileSuccess: false, data: errMsg});
-		})
+				counter++;				
+			})
+			.on('error', errorHandler)
+			.on('close', () => {
+				// console.log("rl close event fired ");
+				resolve({
+					dataName: tempArr
+				});
+			});
+	
+	})
+	.then((jsObj) => {
+		console.log("jsObj ",jsObj.dataName[1]);
+		return({parseTxtFileSuccess: true, data: jsObj});
+	}, (err) => {
+		return Promise.reject({parseTxtFileSuccess:false, data:err});
+	})
 }
 
+// function parseTxtFileToJS (path) {
+// 	let 	lines,
+// 			dataName = path.substring(path.lastIndexOf('/')+1);
+// 	dataName = dataName.substring(0,dataName.lastIndexOf('.'));
+// 	return readFile(path, 'utf8')
+// 		.then((file) => {
+// 			let keys;
+// 			lines = file.toString().split('\r\n');
+
+// 			if (lines && lines.length > 0) {
+// 				// first line shoule be the keys
+// 				keys = convertLineToArray(lines[0]);
+// 				for (let i = 1; i < lines.length; i++) { 
+// 					if (lines[i].length === 0) {
+// 						lines.splice(i,1);
+// 					} else {
+// 						lines[i] = convertLineToArray(lines[i]);						
+// 					}
+// 				}
+// 			}
+// 			let json = convertLinesToJS(lines,keys,dataName);
+// 			return ({parseTxtFileSuccess: true, data: json});
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 			let errMsg = `Error in parseTxtFileToJson readFile: err.code=${err.code} on file ${err.path}.`;
+// 			console.log("errMsg ",errMsg);
+// 			return ({parseTxtFileSuccess: false, data: errMsg});
+// 		})
+// }
+
 function convertLinesToJS(lines,keys,dataName) {
-	// let arr = [];
 	let feedObj = {[dataName]:[]};
 	console.log("convertLinesToJson called on dataName ",dataName);
 	if (lines && lines.length > 0) {
@@ -70,6 +140,6 @@ function convertLineToObj (line, keys) {
 // For testing only
 // let json = parseTxtFileToJson('google_transit/routes.txt');
 // console.log("type of json.then ",typeof json.then);
-// parseTxtFileToJson('google_transit/stop_times.txt');
+parseTxtFileToJS('google_transit/stop_times.txt');
 
 module.exports = parseTxtFileToJS;
