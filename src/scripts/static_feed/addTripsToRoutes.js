602348ede81ebbd1de20afd9d9977946d66ca9c6
eshandro <erik843@gmail.com>
2018-04-03 20:46:03 -0600

    addTripsToRoutes js file added and tested
    Able to add trips to routes successfully.
    Need to test to be sure trip is accurate via populate
    Also need to figure out how to get addTripsToRoutes to return a promise when done

--- src/scripts/static_feed/addTripsToRoutes.js
+++ src/scripts/static_feed/addTripsToRoutes.js
@@ -1,4 +1,48 @@
 const	Trip = require('../../../database/models/trip'),
 		Route = require('../../../database/models/route');
 
-		
+function addTrips(doc) {
+	console.log("addTrips called");
+	console.log("doc in addTrips ",doc);
+	// First clear out existing trips
+	doc.update({trips: []})
+	.then(() => {
+		Trip.find({route_id: doc.route_id})
+		.then((trips) => {
+			doc.trips = trips;
+			return doc.save();
+		})
+	})
+	
+}
+
+
+function addTripsToRoutes() {
+
+	let routesCursor = Route.find({},'route_id trips').cursor();
+	console.log("addTripsToRoutes called")
+	// routesCursor.on('data', (doc) => {
+	// })
+	// routesCursor.on('close', () => {
+	// 	console.log("routesCursor close ");
+	// });
+	routesCursor.eachAsync((doc) => {
+		console.log("eachAsync called");
+		console.log("doc in eachAsync ",doc);
+		// First clear out existing trips
+		doc.update({trips: []})
+		.then(() => {
+			Trip.find({route_id: doc.route_id})
+			.then((trips) => {
+				doc.trips = trips;
+				return doc.save();
+			})
+		})		
+	})
+	.then(() => {
+		console.log('routesCursor done!');
+		return true;
+	})
+
+}
+module.exports = addTripsToRoutes;
--- test/static_feed/addTripsToRoutes_test.js
+++ test/static_feed/addTripsToRoutes_test.js
@@ -1,8 +1,18 @@
 const	Trip = require('../../database/models/trip'),
 		Route = require('../../database/models/route'),
+		addTripsToRoutes = require('../../src/scripts/static_feed/addTripsToRoutes'),
 		assert = require('assert');
 
 describe("Add Trips to each Route", () => {
+	beforeEach((done) => {
+		Route.findOne({route_id: "101C"})
+			.then((route) => {
+				route.update({trips: []})
+				.then(() => {
+					done();
+				})
+			})
+	})
 	it("streams all Route Ids from Route", (done) => {
 		let routesCursor = Route.find({},'route_id').cursor();
 		routesCursor.on('data', (doc) => {
@@ -18,14 +28,12 @@ describe("Add Trips to each Route", () => {
 		Trip.count({})
 			.then((count) => {
 				allTripsCount = count;
-				console.log("allTripsCount ",allTripsCount);
 		
 				Route.findOne({route_id: "101C"})
 					.then((route) => {
 						Trip.count({route_id: route.route_id})
 						.then((count) => {
 							filteredTripsCount = count;
-							console.log("filteredTripsCount ",filteredTripsCount);
 							assert(filteredTripsCount < allTripsCount);
 							done();
 						})
@@ -34,7 +42,27 @@ describe("Add Trips to each Route", () => {
 	})
 
 	it("adds all trips associated with given route to route.trips", (done) => {
-		done()
+		Route.findOne({route_id: "101C"})
+		.then((route) => {
+			Trip.find({route_id: route.route_id})
+			.then((trips) => {
+				route.trips = trips;
+				return route.save();
+			})
+			.then(() => {
+				return Route.findOne({route_id: "101C"}).populate('trips')
+			})
+			.then((route2) => {
+				assert(route2.trips[0].route_id === route.route_id);
+				done();
+			})
+		})
+
+	})
+	it("calls the addTripsToRoutes fn and adds trips to all routes", (done) => {
+		addTripsToRoutes()
+		// .then(data => console.log('data from addTripsToRoutes',data));
+		done();
 	})
 
 })
