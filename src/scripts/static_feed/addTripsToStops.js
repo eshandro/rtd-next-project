8264313cd93a537f40bcfa1cef3f1886c87861b6
eshandro <erik843@gmail.com>
2018-04-07 16:29:39 -0600

    Adding Trips to Stops successfully
    Not sure if the data is correct tho ... stop_times and trips have same length

---
+++ src/scripts/static_feed/addTripsToStops.js
@@ -0,0 +1,37 @@
+const	Trip = require('../../../database/models/trip'),
+		Stop = require('../../../database/models/stop');
+
+
+function getTrips(tripsList) {
+	return Trip.find({trip_id: {$in: tripsList} });
+}
+
+
+function addTripsToStops() {
+	let stopsCursor = Stop.find({}).populate('stop_times','trip_id -_id').cursor();
+
+	return stopsCursor.eachAsync((doc) => {
+		let [tripsList] = [doc.stop_times];
+		// change from array objects to array of just trip_ids
+		tripsList = tripsList.map(item => item.trip_id);
+		// Creates a Set from arr3 and a value in a Set can only occur once
+		// Then, we use the spread operator to turn it back into an Array from a Set
+		tripList = [... new Set(tripsList)];
+		
+		// clear out existing trips before adding trips
+		doc.update({trips: []})
+		.then(() => {
+			return getTrips(tripsList);
+		})
+		.then((trips) => {
+			return doc.update({trips: trips})
+		})
+	
+	})
+	.then(() => {
+		console.log('stopsCursor in addTripsToStops done!');
+		return Promise.resolve({complete: true});
+	})
+
+}
+module.exports = addTripsToStops;
--- test/static_feed/addTripsToStops_test.js
+++ test/static_feed/addTripsToStops_test.js
@@ -1,9 +1,11 @@
 const	Stop = require('../../database/models/stop'),
-		StopTime = require('../../database/models/stop_time'),
-		// addStopTimesToStops = require('../../src/scripts/static_feed/addStopTimesToStops'),
+		// StopTime = require('../../database/models/stop_time'),
+		Trip = require('../../database/models/trip'),
+		addTripsToStops = require('../../src/scripts/static_feed/addTripsToStops'),
 		assert = require('assert');
 
 describe.only("Add Trips to each Stop", () => {
+	let arr1 = ["111859139", "111859169","111859257"];
 	it('can get list of a Stop\'s stop_times', (done) =>{
 		Stop.findOne({stop_id: "25438"})
 		.populate('stop_times')
@@ -12,13 +14,46 @@ describe.only("Add Trips to each Stop", () => {
 			done();
 		})
 	})
-	// it("calls the addStopTimesToStops fn and adds stop_times to all stops", (done) => {
-	// 	addStopTimesToStops()
-	// 	.then((val) => {
-	// 		console.log("val in addStopTimesToStops ",val);
-	// 		done();
-	// 	})
-	// 	// .then(data => console.log('data from addTripsToRoutes',data));
-	// }).timeout(0)
+	it('can get list of trips from array of trip_ids', (done) => {
+		let arr1 = ["111859139", "111859169","111859257"];
+		Trip.find({trip_id: {$in: arr1} }, 'trip_id _id')
+		.then((docs) => {
+			assert(docs.length === arr1.length)
+			done();
+		})
+
+	})
+	it('can make a copy of an array and remove duplicates', (done) => {
+		let [arr2] = [arr1];
+		assert(arr2 === arr1);
+		let arr3 = [...arr2,...arr1];
+		// Creates a Set from arr3 and a value in a Set can only occur once
+		// Then, we use the spread operator to turn it back into an Array from a Set
+		arr3 = [...(new Set(arr3))];
+		assert(arr3.length === new Set(arr3).size)
+
+		done();
+	})
+	xit('streams all stops', (done) => {
+		let counter = 0;
+		let stopsCursor = Stop.find({}).populate('stop_times').cursor();
+		stopsCursor.on('data', (doc) => {
+			if(counter <= 1) console.log("doc ",doc);
+			counter++;
+		})
+		stopsCursor.on('close', ()=> {
+			console.log("stopsCursor close")
+			done();
+		})
+
+	})
+	it("calls the addTripsToStops fn and adds trips to all stops", (done) => {
+		addTripsToStops()
+		.then((val) => {
+			console.log("val in addTripsToStops ",val);
+			done();
+		})
+		// .then(data => console.log('data from addTripsToRoutes',data));
+	}).timeout(0)
 
 })
