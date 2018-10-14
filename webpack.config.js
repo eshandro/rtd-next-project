c32e7b485d6fdbafed69c69e8196d3ad218ddc82
eshandro <erik843@gmail.com>
2018-10-14 10:37:01 -0600

    Get webpack-dev-server to work with plugins and correctly load index.html

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,17 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
-	// template: "./src/index.html",
-	filename: "./dist/index.html"
+	template: path.resolve(__dirname,"src/index.html"),
+	filename: path.resolve(__dirname,"dist/index.html")
 })
-const cleanPlugin = new CleanWebpackPlugin(["dist/scripts"]);
+const cleanPlugin = new CleanWebpackPlugin(["dist"]);
 
 module.exports = {
 	entry: ["./src/client/index.js"],
 	output: {
-		path: path.resolve(__dirname,"dist/scripts/"),
-		publicPath: "scripts/",
-		filename: "bundle.js"
+		path: path.resolve(__dirname,"dist"),
+		publicPath: "/",
+		filename: "scripts/bundle.js"
 	},
 	module: {
 		rules: [
@@ -24,21 +24,26 @@ module.exports = {
 					loader: "babel-loader"
 	    		}
 			},
-			{
-				test: /\.html?/,
-				use: [
-					{
-						loader: "html-loader"
-					}
-				]
-			}
+			// {
+			// 	test: /\.html?/,
+			// 	use: [
+			// 		{
+			// 			loader: "html-loader"
+			// 		}
+			// 	]
+			// }
 		]
 	},
 	devServer: {
-		contentBase: "./dist/",
+		contentBase: path.resolve(__dirname,"dist"),
+		watchContentBase: true,
+		publicPath: "/",
 		proxy: {
 			"/api": "http://localhost:8000"
 		}
 	},
-	plugins: [htmlPlugin]
+	plugins: [
+		 cleanPlugin,
+		 htmlPlugin
+	]
 };
